f3a1eb4913b29f9cd72a6c5d14a6402d41ef23f2
Harsh Pandey <harshp9026@gmail.com>
2026-01-07 17:24:11 +0530

    Style: Replace mobile users icon with a new SVG

--- frontend/src/Chat.tsx
+++ frontend/src/Chat.tsx
@@ -2179,10 +2179,8 @@ function Chat() {
         </GifPickerModal>
       )}
       <Header><HeaderTitle>Pulse</HeaderTitle>            <MobileUserListToggle onClick={() => setIsUserListVisible(!isUserListVisible)}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="currentColor">
-                  <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5 0 00-.75-2.906A3.005 3 0 0119 15v3h-3zM4.75 12.094A5.973 4 0 001 15v3h3v-3a3.005 3 0 013.25-2.906z" />
-                </svg>
-            </MobileUserListToggle></Header>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
+            </MobileUserListToggle>
         <LayoutContainer>
           <ChatWindow>
             <MessagesContainer>
