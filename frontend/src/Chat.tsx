5ca1f75498c55d90b2fbb01d98df7d79871f8ce9
Harsh Pandey <harshp9026@gmail.com>
2026-01-10 20:55:56 +0530

    feat: improve UX for deleted messages and user list
    - Add full stops to the deleted message notifications.
    - Display '(You)' next to the current user's name in the online list.
    - Sort the current user to the top of the online list.

--- frontend/src/Chat.tsx
+++ frontend/src/Chat.tsx
@@ -1314,7 +1314,7 @@ const MessageItem = React.memo(({
         >
           {isDeleted ? (
             <MessageText style={{ fontStyle: 'italic', color: sender === 'me' ? '#bfdbfe' : '#a0aec0' }}>
-              {msg.deletedBy === currentUserId ? 'You deleted this message' : 'This message has been deleted'}
+              {msg.deletedBy === currentUserId ? 'You deleted this message.' : 'This message has been deleted.'}
             </MessageText>
           ) : isEditing ? (
             msg.url ? (
@@ -2375,7 +2375,19 @@ function Chat() {
           </ChatWindow>
           <UserSidebar $isVisible={isUserListVisible}>
             <h2>Online ({onlineUsers.length})</h2>
-            <UserList>{onlineUsers.map((user, index) => <UserListItem key={user.userId} index={index}>{user.username}</UserListItem>)}</UserList>
+            <UserList>
+              {(() => {
+                const currentUser = onlineUsers.find(user => user.userId === userIdRef.current);
+                const otherUsers = onlineUsers.filter(user => user.userId !== userIdRef.current);
+                const sortedUsers = currentUser ? [currentUser, ...otherUsers] : otherUsers;
+                
+                return sortedUsers.map((user, index) => (
+                  <UserListItem key={user.userId} index={index}>
+                    {user.username}{user.userId === userIdRef.current ? ' (You)' : ''}
+                  </UserListItem>
+                ));
+              })()}
+            </UserList>
               <ClearChatButton onClick={handleClearChat}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 9l-6-6H5a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V9z"></path><path d="M15 3v6h6"></path><path d="M9.5 12.5 14.5 17.5"></path><path d="m14.5 12.5-5 5"></path></svg>
                 Clear Chat
